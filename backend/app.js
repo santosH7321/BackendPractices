f4a3541c0592b89b2f98e4adb850acf0896a1756
Santosh Kumar <santoshkumar23kky@gmail.com>
2023-12-01 07:00:10 +0530

    uptoRouts

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const authRouter = require('./router/authRoute');
+
+app.use(express.json());
+app.use('/api/auth', authRouter);
 
 app.use('/',(req,res)=> {
-    res.status(200).json({data: 'jwtauth server'});
+    res.status(200).json({data: 'jwtauth server.'});
 
 });
 
--- backend/index.js
+++ backend/index.js
@@ -2,4 +2,4 @@ const PORT = process.env.PORT || 5000;
 const app = require('./app');
 app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`);
-})
+});
