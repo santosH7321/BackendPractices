c3821594c0f5545e65cf44377293f2acb971b96f
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-10-01 00:00:48 +0530

    Day5

---
+++ NamasteNodejs/Day5/app.js
@@ -0,0 +1,9 @@
+const caculateSum = require('./sum.js');
+
+require('./xyz.js');
+
+caculateSum(5, 10);
+
+var a = 10;
+var b = 20;
+console.log(a+b);
---
+++ NamasteNodejs/Day5/sum.js
@@ -0,0 +1,9 @@
+
+console.log("Sum module executed");
+
+function caculateSum(a, b){
+    const sum = a + b;
+    console.log(sum);
+}
+
+module.exports = caculateSum;
