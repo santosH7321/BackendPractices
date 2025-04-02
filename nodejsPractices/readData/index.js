5d36ec578c3b893846a30267aad8197927df8995
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-04-02 10:11:58 +0530

    route params

--- nodejsPractices/readData/index.js
+++ nodejsPractices/readData/index.js
@@ -10,7 +10,24 @@ app.get("/", (req, res)=> {
 })
 
 app.get("/api/v1/data", (req, res) => {
+    const { name } = req.query;
+    if (name) {
+        const user = Data.filter((user) => {
+            return user.name === name;
+        })
+        res.status(200).send(user);
+    }
     res.status(200).json(Data);
+})
+
+// router params
+app.get("/api/v1/data/:id", (req, res) => {
+    const { id } = req.params;
+    // console.log(id);
+    const user = Data.filter((user) => {
+        return user.id === parseInt(id);
+    })
+    res.status(200).send(user);
 })
 app.listen(PORT, (req, res) => {
     console.log(`Server is running on port ${PORT}`);
