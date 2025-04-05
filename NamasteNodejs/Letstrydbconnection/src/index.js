28ab798b145bb38d8b3b80d08caf1bc21503031b
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-04-05 13:09:54 +0530

    well done also added user successfully

--- NamasteNodejs/Letstrydbconnection/src/index.js
+++ NamasteNodejs/Letstrydbconnection/src/index.js
@@ -1,11 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "../Database/database.js";
+import User from "../Models/userSchema.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+app.post("/api/v1/signup", async (req, res) => {
+    const userObj = new User({
+        firstName: "SKY",
+        lastName: "Ranjan",
+        email: "skys@gmail.com",
+    });
+    await userObj.save();
+    res.status(201).json({ message: "User created successfully" });
+})
+
+
 
 connectDB()
   .then(() => {
