f3b6d46a2e4c67aafe897870d0992428712a5d1c
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-04-05 12:47:08 +0530

    create data base and on post call saving the data

--- NamasteNodejs/Database/src/index.js
+++ NamasteNodejs/Database/src/index.js
@@ -1,11 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "../config/database.js";
+import User from "./models/user.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Nishant",
+    lastName: "Ranjan",
+    email: "nishant@gmail.com",
+    password: "nishant@123",
+    age: 35,
+    gender: "male",
+  });
+
+  try {
+    await user.save();
+    res.status(201).send("User added successfully");
+  } catch (error) {
+    res.status(400).send("Error saving the user",error.message);
+  }
+
+  
+});
+// ROutes
 
 // Database connect befor the server starts
 connectDB()
