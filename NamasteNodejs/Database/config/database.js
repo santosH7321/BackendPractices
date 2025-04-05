7d73dd389fc4b2f142c372731803ccc4e7cbbc69
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-04-05 11:48:44 +0530

    good practice to connect database

--- NamasteNodejs/Database/config/database.js
+++ NamasteNodejs/Database/config/database.js
@@ -7,10 +7,4 @@ export const connectDB = async () => {
   await mongoose.connect(process.env.MONGODB_URI);
 };
 
-connectDB()
-  .then(() => {
-    console.log("Database connected successfully");
-  })
-  .catch((err) => {
-    console.error("Database connection failed", err);
-  });
+
--- NamasteNodejs/Database/src/index.js
+++ NamasteNodejs/Database/src/index.js
@@ -7,6 +7,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Database connect befor the server starts
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Database connection failed", err);
+  });
