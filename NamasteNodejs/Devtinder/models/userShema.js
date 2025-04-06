83d4c7f3fef832774f04de305308b9eebea94050
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-04-06 08:33:18 +0530

    npm validator

--- NamasteNodejs/Devtinder/models/userShema.js
+++ NamasteNodejs/Devtinder/models/userShema.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import validator from "validator";
+
 
 const userSchema = new mongoose.Schema({
     firstName:{
@@ -16,11 +18,17 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
         match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid email format.");
+            }
+        }
     },
     password: {
         type: String,
         required: true,
         minlength: 6,
+        
     },
     gender: {
         type: String,
@@ -38,6 +46,11 @@ const userSchema = new mongoose.Schema({
     photoUrl: {
         type: String,
         default: "https://example.com/default-avatar.png",
+        validate(value) {
+            if (!validator.isURL(value)) {
+                throw new Error("Invalid photo URL format.");
+            }
+        }
     },
 
 }, { timestamps: true });
