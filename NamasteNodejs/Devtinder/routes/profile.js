546d02f6a3c086fc4d9d78534b32c2c58f97c460
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-04-10 10:48:43 +0530

    forgotPassword but some error in this

--- NamasteNodejs/Devtinder/routes/profile.js
+++ NamasteNodejs/Devtinder/routes/profile.js
@@ -1,6 +1,7 @@
 import express from "express";
 import UserAuth from "../middleware/auth.js";
 import { validateEditProfileData } from "../utils/validation.js";
+import User from "../models/userShema.js";
 
 const profileRouter = express.Router();
 
@@ -22,13 +23,38 @@ profileRouter.patch("/api/v1/profile/edit", UserAuth, async (req, res) => {
     Object.keys(req.body).forEach((key) => {
       loggedInUser[key] = req.body[key];
     });
-    
+
     await loggedInUser.save();
 
-    res.status(200).send(`${loggedInUser.firstName} your profile is updated successfully!`);
+    res.status(200).json({
+      message: `${loggedInUser.firstName}, your profile is updated successfully!`,
+      data: loggedInUser,
+    });
   } catch (error) {
     res.status(400).send("Invalid Edit request!");
   }
 });
+
+profileRouter.patch(
+  "/api/v1/profile/forgotpassword",
+  UserAuth,
+  async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+
+      if (user) {
+        const passwordHash = await bcrypt.hash(password, 10);
+        user.password = passwordHash;
+        await user.save();
+        res.status(200).send("Password updated successfully!");
+      } else {
+        res.status(400).send("Invalid email address!");
+      }
+    } catch (error) {
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 export default profileRouter;
