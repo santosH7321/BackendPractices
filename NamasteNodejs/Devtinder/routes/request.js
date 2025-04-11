a0c217dea303d7a13a8004ebb0777e102fb9af58
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-04-11 07:28:20 +0530

    send Connection request

--- NamasteNodejs/Devtinder/routes/request.js
+++ NamasteNodejs/Devtinder/routes/request.js
@@ -1,10 +1,33 @@
 import express from "express";
 import UserAuth from "../middleware/auth.js";
+import ConnectionRequestModel from "../models/connectionRequest.js";
 
 const requestRouter = express.Router();
 
-requestRouter.post("/api/v1/sendConnectionRequest", UserAuth, async (req, res) => {
-    const user = req.user;
+requestRouter.post(
+  "/request/send/:status/:toUserId",
+  UserAuth,
+  async (req, res) => {
+    try {
+      const fromUserId = req.user._id;
+      const toUserId = req.params.toUserId;
+      const status = req.params.status;
+
+      const ConnectionRequest = new ConnectionRequestModel({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data = await ConnectionRequest.save();
+      res.json({
+        message: "Connection request sent successfully",
+        data: data,
+      });
+
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
     res.send(user.firstName + " " + "Sending a connection request");
   }
 );
