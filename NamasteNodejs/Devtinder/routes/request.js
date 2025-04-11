3bee7bb93b1630ea1cf88e3e0879452f76508655
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-04-11 23:43:00 +0530

    Connection request accepted and rejected

--- NamasteNodejs/Devtinder/routes/request.js
+++ NamasteNodejs/Devtinder/routes/request.js
@@ -34,8 +34,6 @@ requestRouter.post(
         });
       }
 
-      
-
       const toUser = await User.findById(toUserId);
       if (!toUser) {
         return res.status(400).json({
@@ -51,7 +49,44 @@ requestRouter.post(
 
       const data = await ConnectionRequest.save();
       res.json({
-        message: req.user.firstName + " is " + status + " in " + toUser.firstName,
+        message:
+          req.user.firstName + " is " + status + " in " + toUser.firstName,
+        data: data,
+      });
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+  }
+);
+
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  UserAuth,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+      const { status, requestId } = req.params;
+      const allwoedStatus = ["accepted", "rejected"];
+      if (!allwoedStatus.includes(status)) {
+        return res.status(400).json({
+          message: "Invalid status type: " + status,
+        });
+      }
+      const connectionRequest = await ConnectionRequestModel.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+
+      if (!connectionRequest) {
+        return res.status(404).json({
+          message: "Connection request not found",
+        });
+      }
+      connectionRequest.status = status;
+      const data = await connectionRequest.save();
+      res.json({
+        message: loggedInUser.firstName + " has " + status + " the request",
         data: data,
       });
     } catch (error) {
