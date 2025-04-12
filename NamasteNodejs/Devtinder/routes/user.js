51050d67c0e39a8d7fe47abd669410ac5ca485da
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-04-12 07:32:57 +0530

    Connection request Accept and reject handeller

--- NamasteNodejs/Devtinder/routes/user.js
+++ NamasteNodejs/Devtinder/routes/user.js
@@ -3,6 +3,8 @@ import UserAuth from "../middleware/auth.js";
 import ConnectionRequestModel from "../models/connectionRequest.js";
 const userRouter = express.Router();
 
+
+const USER_SAFE_DATA = "fromUserId firstName lastName photoUrl skills gender about";
 // Get all pending connection requests for the logged-in user
 userRouter.get("/user/requests/received", UserAuth, async (req, res) => {
   try {
@@ -15,6 +17,28 @@ userRouter.get("/user/requests/received", UserAuth, async (req, res) => {
       status: true,
       message: "Connection requests fetched successfully",
       data: connectionRequest,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+userRouter.get("/user/connections",UserAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequest = await ConnectionRequestModel.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId", USER_SAFE_DATA);
+
+    const data = connectionRequest.map((row) => row.fromUserId); 
+
+    res.status(200).json({
+      status: true,
+      message: "Connections fetched successfully",
+      data,
     });
   } catch (error) {
     throw new Error(error);
