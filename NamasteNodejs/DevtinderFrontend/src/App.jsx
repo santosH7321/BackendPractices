1921319e6c1293ee4bf39622cfd3115850174dcb
Santosh Kumar <santoshkumar23kky@gmail.com>
2025-04-13 11:40:03 +0530

    tailwind setup

--- NamasteNodejs/DevtinderFrontend/src/App.jsx
+++ NamasteNodejs/DevtinderFrontend/src/App.jsx
@@ -3,9 +3,9 @@ function App() {
 
 
   return (
-    <>
+    <head className="flex justify-center items-center h-screen bg-gray-300 text-4xl font-bold ">
       <h1>Hello world</h1>
-    </>
+    </head>
   )
 }
 
